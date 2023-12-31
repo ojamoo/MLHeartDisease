bd2aef362b8d1f422a0b11ea34b5c72e6fbafa11
Olamide Amoo <85574944+ojamoo@users.noreply.github.com>
2023-12-31 01:38:36 +0000

    Update request.py

--- request.py
+++ request.py
@@ -1,6 +1,6 @@
 import requests
 
-url = 'http://localhost:5000/predict_api'
+url = 'http://127.0.0.1:5000'
 r = requests.post(url,json={'age': 40, 'Sex':1, 'ChestPainType':1, 'RestingBP':140, 'Cholesterol': 289, 'FastingBS':0, 'RestingECG':1, 'MaxHR':172, 'ExerciseAngina': 0, 'Oldpeak':0.0, 'ST_Slope':2})
 
 print(r.json())
